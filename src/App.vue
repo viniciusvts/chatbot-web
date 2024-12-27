917e5e55b38969fbac53c1cf8294c0b9bb16a096
Vinicius de Santana <vinicius.vts@gmail.com>
2024-12-27 15:07:56 -0300

    feat: add bootstrap, sass and remove vue unused files

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,25 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <nav class="navbar navbar-dark bg-primary px-3 pb-0">
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <RouterLink class="nav-link" :class="{ active: $route.name === 'home' }" :to="{ name: 'home' }">Home</RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink class="nav-link" :class="{ active: $route.name === 'chat' }" :to="{ name: 'chat' }">Chat</RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink class="nav-link" :class="{ active: $route.name === 'about' }" :to="{ name: 'about' }">About</RouterLink>
+      </li>
+    </ul>
+  </nav>
+  <header class="container">
+    <div class="row">
     </div>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/views/ChatView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <h1>Chat view</h1>
+    </div>
+  </div>
+</template>
