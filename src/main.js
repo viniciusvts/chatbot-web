cccbf60aaf109fbfb1b6f8ad362e77ccc704fafc
Vinicius de Santana <vinicius.vts@gmail.com>
2024-12-27 18:51:16 -0300

    feat: add pinia data storage

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import './assets/style/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 import App from './App.vue'
 import router from './router'
 import 'bootstrap';
 
 const app = createApp(App)
+const pinia = createPinia();
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
